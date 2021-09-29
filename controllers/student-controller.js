34c3d521f8a91d1346b4966039f3d0260336f385
chiragjain99991 <chiragjain55553@gmail.com>
2021-09-29 20:37:02 +0530

    changes in student-controller page

--- controllers/student-controller.js
+++ controllers/student-controller.js
@@ -1,4 +1,5 @@
 const StudentService = require("./services/student-service")
+const Student = require("../models/Student")
 class StudentControler{
 
     async createStudent(req, res){
@@ -8,8 +9,12 @@ class StudentControler{
         }
         let student;
         try{
+
+            //to check if the id provided is already associated to one of the user in the system
             const findStudent = await StudentService.findStudentById({id})
             if(findStudent){
+
+                //As Id should be unique
                 return res.status(400).json({msg:`User with id ${id} already exists`})
             }else{
                 student = await StudentService.create({name, division, id, year})
@@ -70,6 +75,20 @@ class StudentControler{
         }
         studentId = String(studentId)
         try{      
+
+                // If id from params is not same as id from request of body and if user is trying to change the id to the one which is already present in the system
+                if(String(studentId) !== String(id)){
+                    const student = await Student.findOne({ id });
+                    if(student){
+
+                        // As Id is unique so the user will not be updated
+                        return res.status(400).json({msg:`User with id ${id} already exists`})
+                    }
+                }
+
+                //If id from params is same as id from request of body or if user is trying to change the id to the one which is not present in the system 
+                // so as to maintain the uniqueness of id 
+                
                 const student = await StudentService.update({studentId, name, division, id, year});
                 if(student){
                     return res.status(200).json({msg:"user updated successfully", student})
